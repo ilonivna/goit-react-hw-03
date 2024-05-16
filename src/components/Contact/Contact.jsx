f1e7cb8a25701f388cf46a898427d2493eb4a2df
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-05-16 21:47:37 +0300

    icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,16 @@
 import css from "./Contact.module.css"
+import { IoPerson } from "react-icons/io5";
+import { MdLocalPhone } from "react-icons/md";
+
+
 
 export default function Contact({ data: { id, name, number }, onDelete }) {
     return (
         <div className={css.contact}>
             <div>
-            <p>{name}</p>
-            <p>{number}</p></div>
+            <p><IoPerson />{name}</p>
+            <p><MdLocalPhone />
+{number}</p></div>
             <button className={css.btn} onClick={() => onDelete(id)}>Delete</button>
         </div>
     )
